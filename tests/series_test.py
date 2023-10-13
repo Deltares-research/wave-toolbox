7d75839b2fcf4c0173aa8b206fece7fc275584a9
Joost den Bieman <jpdenbieman@gmail.com>
2023-10-13 20:10:56 +0200

    gather communal fixtures in conftest.py
    so they can be used in multiple test files

--- tests/series_test.py
+++ tests/series_test.py
@@ -1,39 +1,6 @@
 import numpy as np
 import pytest
 
-from deltares_wave_toolbox.cores.core_wavefunctions import create_spectrum_jonswap
-
-
-@pytest.fixture()
-def Hm0():
-    """Hm0 value to be used in tests."""
-
-    Hm0 = 2.0
-
-    return Hm0
-
-
-@pytest.fixture()
-def wave_spectrum(Hm0):
-    """Generate wave spectrum to be used in tests."""
-
-    # create JONSWAP Spectrum
-    ff = np.linspace(0.01, 2, 1000)
-
-    spectrum = create_spectrum_jonswap(f=ff, fp=0.1, hm0=Hm0)
-
-    return spectrum
-
-
-@pytest.fixture()
-def wave_timeseries(wave_spectrum):
-    """Generate wave timeseries to be used in tests."""
-
-    # create Series from spectrum
-    timeseries = wave_spectrum.create_series(10, 3600, 0.1)
-
-    return timeseries
-
 
 def test_get_H2p_Rayleigh(wave_spectrum, wave_timeseries):
     """Test the get_H2p_Rayleigh function."""
--- tests/spectrum_test.py
+++ tests/spectrum_test.py
@@ -1,39 +1,4 @@
-import numpy as np
 import pytest
-
-from deltares_wave_toolbox.cores.core_wavefunctions import create_spectrum_jonswap
-from deltares_wave_toolbox.cores.core_spectral import compute_spectrum_freq_serie
-
-
-@pytest.fixture()
-def Hm0():
-    """Hm0 value to be used in tests."""
-
-    Hm0 = 2.0
-
-    return Hm0
-
-
-@pytest.fixture()
-def wave_spectrum(Hm0):
-    """Generate wave spectrum to be used in tests."""
-
-    # create JONSWAP Spectrum
-    ff = np.linspace(0.01, 2, 1000)
-
-    spectrum = create_spectrum_jonswap(f=ff, fp=0.1, hm0=Hm0)
-
-    return spectrum
-
-
-@pytest.fixture()
-def wave_timeseries(wave_spectrum):
-    """Generate wave timeseries to be used in tests."""
-
-    # create Series from spectrum
-    timeseries = wave_spectrum.create_series(10, 3600, 0.1)
-
-    return timeseries
 
 
 def test_get_Hm0(Hm0, wave_spectrum):
