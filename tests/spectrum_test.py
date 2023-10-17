ae8ce7a9cd396a57003c310d7046ba07f966f40a
Joost den Bieman <jpdenbieman@gmail.com>
2023-10-17 21:21:07 +0200

    Update spectrum_test.py
    add test for Tp (which isn't reproduced that accurately!)

--- tests/spectrum_test.py
+++ tests/spectrum_test.py
@@ -2,6 +2,9 @@ import pytest
 
 
 def test_get_Hm0(Hm0, wave_spectrum):
-    """Test the get_Hm0 function."""
-
     assert wave_spectrum.get_Hm0() == pytest.approx(Hm0)
+
+
+# TODO fails for rel=1e-6, check whether frequency discretization isn't too coarse
+def test_get_Tp(Tp, wave_spectrum):
+    assert wave_spectrum.get_Tp() == pytest.approx(Tp, rel=1e-2)
