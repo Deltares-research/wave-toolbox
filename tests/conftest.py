5eba6acb923204e4ef1e7c32c510cebe518a9b0a
Joost den Bieman <jpdenbieman@gmail.com>
2023-10-18 10:17:19 +0200

    remove redundant comments/descriptions

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,39 +6,24 @@ from deltares_wave_toolbox.cores.core_wavefunctions import create_spectrum_jonsw
 
 @pytest.fixture(scope="package")
 def Hm0():
-    """Hm0 value to be used in tests."""
-
     Hm0 = 2.0
-
     return Hm0
 
 
 @pytest.fixture(scope="package")
 def Tp():
-    """Hm0 value to be used in tests."""
-
     Tp = 10.0
-
     return Tp
 
 
 @pytest.fixture(scope="package")
 def wave_spectrum(Hm0, Tp):
-    """Generate wave spectrum to be used in tests."""
-
-    # create JONSWAP Spectrum
     ff = np.linspace(0.01, 2, 1000)
-
     spectrum = create_spectrum_jonswap(f=ff, fp=1 / Tp, hm0=Hm0)
-
     return spectrum
 
 
 @pytest.fixture(scope="package")
 def wave_timeseries(wave_spectrum):
-    """Generate wave timeseries to be used in tests."""
-
-    # create Series from spectrum
     timeseries = wave_spectrum.create_series(10, 3600, 0.1)
-
     return timeseries
--- tests/series_test.py
+++ tests/series_test.py
@@ -3,8 +3,6 @@ import pytest
 
 
 def test_get_H2p_Rayleigh(wave_spectrum, wave_timeseries):
-    """Test the get_H2p_Rayleigh function."""
-
     H2p = wave_timeseries.get_Hs()[0] * np.sqrt(-np.log(0.02)) / np.sqrt(2)
 
     assert wave_timeseries.get_H2p_Rayleigh() == pytest.approx(H2p)
