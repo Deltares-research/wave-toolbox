6f1e611293eda5cf0ea57fb4ad2245501ff685db
Joost den Bieman <jpdenbieman@gmail.com>
2023-11-01 12:43:59 +0100

    try PyData theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,6 +37,7 @@ extensions = [
     "sphinx.ext.viewcode",
     "sphinx.ext.napoleon",
     "sphinx_rtd_theme",
+    "pydata_sphinx_theme",
     "nbsphinx",
 ]
 
@@ -90,7 +91,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+html_theme = "pydata_sphinx_theme"
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
