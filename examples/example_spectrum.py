04e22f3ab8cd984ff2569f9c492a25e04bea87ac
menno94 <menno-deridder@hotmail.com>
2023-11-02 10:29:57 +0100

    adjust example script

--- examples/example_spectrum.py
+++ examples/example_spectrum.py
@@ -7,34 +7,36 @@ sys.path.insert(1, os.getcwd())
 
 import deltares_wave_toolbox as dwt
 
-# create JONSWAP Spectrum ##
+## create JONSWAP Spectrum ##
 ff = np.linspace(0.01, 2, 1000)
 
 spec = dwt.core_wavefunctions.create_spectrum_object_jonswap(f=ff, fp=0.1, hm0=2)
 
-
+## compute wave parameters ##
 Hm0 = spec.get_Hm0()
 Tps = spec.get_Tps()
 Tmm10 = spec.get_Tmm10()
 
 spec.plot(xlim=[0, 0.5])
 
-# create Series from spectrum ##
+## create Series from spectrum ##
 timeseries = spec.create_series(10, 3600, 0.1)
 
 timeseries.plot(plot_crossing=True)
 
-
+## get fourier comp ##
 f, xFreq, isOdd = timeseries.get_fourier_comp()
 
 plt.figure()
 plt.plot(f, xFreq)
 
+## transform back to spectrum ##
 [ff, ss] = dwt.core_spectral.compute_spectrum_freq_series(f, xFreq, timeseries.nt, 0.01)
 
 plt.figure()
 plt.plot(ff, ss)
 
+## time series analysis ##
 var = timeseries.var()
 
 
@@ -59,7 +61,7 @@ print("--- wave exceedance ---")
 print("h2perc:", h2perc)
 print("h10perc:", h10perc)
 
-spec2 = timeseries.get_spectrum(fres=0.01)
+spec2 = timeseries.get_spectrum()
 
 spec2.plot()
 
