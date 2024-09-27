ed361c67b3bd90f4d29f237f37e684ce144b903b
Joost den Bieman <jpdenbieman@gmail.com>
2024-09-27 10:24:59 +0200

    restructure

--- deltares_wave_toolbox/__init__.py
+++ deltares_wave_toolbox/__init__.py
@@ -1,12 +1,13 @@
+from .cores import *  # noqa
 from .series import Series, WaveHeights  # noqa
 from .spectrum import Spectrum  # noqa
-from .cores import *  # noqa
 
 __version__ = "1.0.0"
 
+import os
+
 # set stylesheet
 import matplotlib.pyplot as plt
-import os
 
 plt.style.use(
     os.path.join(os.path.dirname(os.path.abspath(__file__)), "Style.mplstyle")
